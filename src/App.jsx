15152c311a523c9b91e913d33f7cb665bd9903ee
unknown <vijaysudhakar760@gmail.com>
2025-08-17 19:44:12 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import { Home } from "./Components/Home/Home";
 import { Header } from "./Components/Headers/Header";
 import { Cart } from "./Components/cart/Cart";
-import { PageNotFound } from "./Components/pageNotFound/pageNotFound";
-import { Wishlist } from "./Components/wishlist/wishlist";
+import { PageNotFound } from "./Components/pageNotFound/PageNotFound";
+import { Wishlist } from "./Components/wishlist/Wishlist";
 
 
 
